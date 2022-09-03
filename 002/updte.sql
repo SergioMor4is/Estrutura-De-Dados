2af5c25d45748bb1d7a55e87c67197e5a75e8b1d
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-03 18:39:54 -0300

    Create updte.sql

---
+++ 002/updte.sql
@@ -0,0 +1,21 @@
+create table if not exists cursos (
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2022'
+) default charset=utf8;
+
+
+alter table cursos
+add column idcurso int first;
+
+describe cursos;
+
+
+alter table cursos
+add primary key (idcurso);
+
+
+
+
