b631c233436afe6d473044311eb466a65c78576d
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-03 17:28:47 -0300

    Update index.sql

--- 001/index.sql
+++ 001/index.sql
@@ -12,3 +12,13 @@ altura decimal(3,2),
 nacionalidade varchar(20) default'Brasil',
 primary key (id)
 )default charset = utf8;
+
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Max', '2009-05-11', 'F', '44', '1.56', 'Brasileira'),
+('Nathalia', '1999-07-28', 'F', '86', '1.72', 'Brasileira'),
+('Renan', '2007-12-12', 'M', '52', '1.74', 'Brasileira');
+
+select * from pessoas;
+
