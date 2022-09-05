43ce11828e0f06d7de8e1201696e73c643a56adb
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-05 10:53:47 -0300

    Update index.sql

--- aulas/001/index.sql
+++ aulas/001/index.sql
@@ -10,31 +10,32 @@ sexo enum('M', 'F'),
 peso decimal(5,2),
 altura decimal(3,2),
 nacionalidade varchar(20) default'Brasil',
-primary key (id)
+PRIMARY KEY(id)
 )default charset = utf8;
 
 insert into pessoas
 (nome, nascimento, sexo, peso, altura, nacionalidade)
 values
-('Max', '2009-05-11', 'U', '44', '1.56', 'Brasileira'),
+('Sergio', '1998-04-13', 'M', '89', '1.84', 'Brasileira'),
+('Max', '2009-05-11', 'M', '44', '1.56', 'Brasileira'),
 ('Nathalia', '1999-07-28', 'F', '86', '1.72', 'Brasileira'),
 ('Renan', '2007-12-12', 'M', '52', '1.74', 'Brasileira');
 
 select * from pessoas;
 
-alter table pessoas
-add column profissao varchar(10);
+ALTER TABLE pessoas
+ADD COLUMN profissao varchar(10);
 
-alter table pessoas
+ALTER TABLE pessoas
+MODIFY COLUMN sexo enum('M', 'F', 'U') after nascimento;
+
+ALTER TABLE pessoas
 drop column sexo;
-
-alter table pessoas
-add column sexo enum('M', 'F', 'U') after nascimento;
-
 
 alter table pessoas
 modify column profissao varchar(20) not null default '';
 
+drop table pessoas;
 
  
 
